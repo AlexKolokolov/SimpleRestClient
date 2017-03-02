6bc6ab0317688729f319d4a3992c6f129caeb0ab
kolokolov <kolokolov_a@ukr.net>
2017-03-02 12:13:54 +0200

    Add new employee registering ability to index.html

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function(){
     $.getJSON('http://localhost:8080/SimpleProject/webapi/employees', function(employees){
     	console.log(employees);
     	$.each(employees, function(i, employee){
-    		console.log(employee);
+    		// console.log(employee);
     		var $employeeTable = $('.employees');
     		$employeeTable.append('<tr><td>' + employee.id 
     			+ '</td><td>' + employee.firstName 
@@ -11,4 +11,29 @@ $(function(){
     			+ '</td><td><a href="employee.html?id=' + employee.id + '"><button>Details</button></a></td></tr>');
     	});
     });
+
+    $('form').submit(function(event){
+        var data = $('form').serializeArray();
+
+        var jsonEmployee = arrayToJson(data);
+        console.log(jsonEmployee);
+        $.ajax({
+            url: "http://localhost:8080/SimpleProject/webapi/employees",
+            type: "POST",
+            data: jsonEmployee,
+            dataType: "application/json",
+            contentType: "application/json"
+        });
+        console.log("Employe " + jsonEmployee + " has been posted");
+
+        event.preventDefault();
+    });
+
+    function arrayToJson(array) {
+        var object = {};
+        $.map(array, function(n, i) {
+            object[n['name']] = n['value'];
+        });
+        return JSON.stringify(object);
+    }
 });
